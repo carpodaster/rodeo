eaca237b9deb3d4e5b97d28027b13694e5a8a267
Carsten Zimmermann <cz@aegisnet.de>
2017-04-23 23:49:16 +0200

    Document default handler

--- lib/rodeo/handler/default.ex
+++ lib/rodeo/handler/default.ex
@@ -1,5 +1,12 @@
 defmodule Rodeo.Handler.Default do
+
+  @moduledoc "A handler returning 'Hello World' as plain text response."
+
   use Rodeo.Handler
+
+  @doc false
   def body(_), do: "Hello World"
+
+  @doc false
   def headers(_), do: [{"content-type", "text/plain; charset=utf-8"}]
 end
